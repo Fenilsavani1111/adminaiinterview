8062214f1c9d50db695188b4e9f9036d9eb56883
Surbhi Vasoya <vasoyasurbhi@gmail.com>
2025-07-18 21:56:26 +0530

    generate token for whatsapp and linkedin url, candidate to studentwithjobposts from direct url

--- src/components/JobPostManager.tsx
+++ src/components/JobPostManager.tsx
@@ -15,6 +15,7 @@ import {
   Mail,
   CheckCircle,
   Linkedin,
+  Link,
 } from "lucide-react";
 import { useApp } from "../context/AppContext";
 import { JobApplicationsList } from "./JobApplicationsList";
@@ -44,6 +45,21 @@ export function JobPostManager() {
     email: "",
   });
   const [shareEmailLoading, setShareEmailLoading] = useState(false);
+  const phoneRegex = /^(?:\+91|91)?[6-9]\d{9}$/;
+  const [whatsAppPopupData, setWhatsAppPopupData] = useState<{
+    showPopup: boolean;
+    jobId: string;
+    phone_number: string;
+    isValid: boolean;
+    loading: boolean;
+  }>({
+    showPopup: false,
+    jobId: "",
+    phone_number: "",
+    isValid: true,
+    loading: false,
+  });
+
   // Add dummy values for missing fields from API
   const processedJobPosts = jobPosts.map((job) => ({
     ...job,
@@ -125,6 +141,49 @@ export function JobPostManager() {
       setTimeout(() => setCopiedUrl(null), 2000);
     } catch (err) {
       console.error("Failed to copy URL:", err);
+    }
+  };
+
+  const getJobLink = async (
+    jobId: string,
+    type: "open" | "whatsapp" | "linkedin" | "monstar"
+  ) => {
+    try {
+      let response = await jobPostAPI.generateTokenForJobInterviewLink(jobId);
+      if (response?.token?.length > 0) {
+        let interviewlink = `https://aiinterview.deepvox.ai/?token=${response?.token}`;
+        // let interviewlink = `http://localhost:5173/?token=${response?.token}`;
+        if (type === "open") {
+          window.open(interviewlink, "_blank");
+        } else if (type === "whatsapp") {
+          setWhatsAppPopupData({
+            ...whatsAppPopupData,
+            loading: true,
+          });
+          const whatsappURL = `https://wa.me/${
+            whatsAppPopupData?.phone_number
+          }?text=${encodeURIComponent(
+            `Hi! Here is a link for interview: ${interviewlink}`
+          )}`;
+          window.open(whatsappURL, "_blank");
+          setWhatsAppPopupData({
+            showPopup: false,
+            jobId: "",
+            phone_number: "",
+            isValid: true,
+            loading: false,
+          });
+        } else if (type === "linkedin") {
+          const linkedinURL = `https://www.linkedin.com/sharing/share-offsite/?url=${encodeURIComponent(
+            interviewlink
+          )}`;
+          window.open(linkedinURL, "_blank");
+        } else if (type === "monstar") {
+        }
+      }
+    } catch (error) {
+      console.log("Failed to open job Link:", error);
+      alert("Failed to open job Link. Please try again.");
     }
   };
 
@@ -478,6 +537,15 @@ Best regards`);
                             )}
                           </button>
                           <button
+                            onClick={() => {
+                              getJobLink(job.id, "open");
+                            }}
+                            title="Open Link"
+                            className="text-gray-600 hover:text-gray-900"
+                          >
+                            <Link className="h-4 w-4" />
+                          </button>
+                          <button
                             onClick={() => setShareModalOpen(job.id)}
                             title="Share"
                             className="text-gray-600 hover:text-gray-900"
@@ -485,6 +553,33 @@ Best regards`);
                             <Share2 className="h-4 w-4" />
                           </button>
                           <button
+                            onClick={() => {
+                              // getJobLink(job.id, "whatsapp");
+                              setWhatsAppPopupData({
+                                showPopup: true,
+                                jobId: job.id,
+                                phone_number: "",
+                                isValid: true,
+                                loading: false,
+                              });
+                            }}
+                            title="Whatsapp"
+                            className="text-gray-600 hover:text-gray-900"
+                          >
+                            <svg
+                              viewBox="0 0 448 512"
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="currentColor"
+                              stroke="currentColor"
+                              className="size-4"
+                            >
+                              <path d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7.9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z"></path>
+                            </svg>
+                          </button>
+                          <button
+                            onClick={() => {
+                              getJobLink(job.id, "linkedin");
+                            }}
                             title="LinkedIn"
                             className="text-gray-600 hover:text-gray-900"
                           >
@@ -642,6 +737,93 @@ Best regards`);
                         onClick={() => {
                           setFormData({ email: "" });
                           setShareModalOpen(null);
+                        }}
+                        className="w-full flex items-center justify-center space-x-2 px-4 py-2 border border-gray-300 rounded-lg hover:bg-gray-50 transition-colors"
+                      >
+                        Close
+                      </button>
+                    </>
+                  )}
+                </div>
+              );
+            })()}
+          </div>
+        </div>
+      )}
+
+      {/* whatsapp Modal */}
+      {whatsAppPopupData.showPopup && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+          <div className="bg-white rounded-2xl p-8 max-w-md w-full mx-4">
+            <h3 className="text-xl font-bold text-gray-900 mb-4">
+              Share Job Post with Whatsapp
+            </h3>
+
+            {(() => {
+              const job = processedJobPosts.find(
+                (j) => j.id === whatsAppPopupData.jobId
+              );
+              if (!job) return null;
+
+              return (
+                <div className="space-y-4">
+                  <div className="bg-gray-50 p-4 rounded-lg">
+                    <h4 className="font-medium text-gray-900">{job.title}</h4>
+                    <p className="text-sm text-gray-600">
+                      {job.company} • {job.location}
+                    </p>
+                  </div>
+
+                  <div className="space-y-3">
+                    <div>
+                      <label className="block text-sm font-medium text-gray-700 mb-2">
+                        Phone Number *
+                      </label>
+                      <input
+                        type="text"
+                        value={whatsAppPopupData.phone_number}
+                        onChange={(e) => {
+                          const value = e.target.value;
+                          setWhatsAppPopupData({
+                            ...whatsAppPopupData,
+                            phone_number: value,
+                            isValid: phoneRegex.test(value),
+                          });
+                        }}
+                        // maxLength={10}
+                        className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                        placeholder="Enter Mobile number"
+                      />
+                      {!whatsAppPopupData.isValid && (
+                        <p className="text-red-500 text-sm">
+                          Please enter valid phone number
+                        </p>
+                      )}
+                    </div>
+                  </div>
+                  {whatsAppPopupData.loading ? (
+                    <div className="flex justify-center mt-4">
+                      <div className="h-6 w-6 border-4 border-gray-900 border-t-transparent rounded-full animate-spin"></div>
+                    </div>
+                  ) : (
+                    <>
+                      <button
+                        onClick={() => {
+                          getJobLink(whatsAppPopupData.jobId, "whatsapp");
+                        }}
+                        className="w-full px-4 py-2 bg-gray-900 text-white rounded-lg hover:bg-gray-800 transition-colors"
+                      >
+                        Submit
+                      </button>
+                      <button
+                        onClick={() => {
+                          setWhatsAppPopupData({
+                            showPopup: false,
+                            jobId: "",
+                            phone_number: "",
+                            isValid: true,
+                            loading: false,
+                          });
                         }}
                         className="w-full flex items-center justify-center space-x-2 px-4 py-2 border border-gray-300 rounded-lg hover:bg-gray-50 transition-colors"
                       >
