0a66aa5117c159538ca9d1cb05cf9a3a48875680
Surbhi Vasoya <vasoyasurbhi@gmail.com>
2025-09-05 20:19:15 +0530

    fix

--- src/components/CandidatePerformanceDetail.tsx
+++ src/components/CandidatePerformanceDetail.tsx
@@ -358,15 +358,14 @@ export function CandidatePerformanceDetail({
                           Questions Answered
                         </div>
                         <div className="flex justify-center mt-2">
-                          {[
-                            ...Array(
-                              candidateData?.StudentInterviewAnswer?.length
-                            ),
-                          ].map((_, i) => (
+                          {[...Array(5)].map((_, i) => (
                             <Star
                               key={i}
                               className={`h-4 w-4 ${
-                                i < candidateData?.attemptedQuestions
+                                i <
+                                Math.round(
+                                  (attemptedQuestions / totalQuestions) * 5
+                                )
                                   ? "text-yellow-500 fill-current"
                                   : "text-gray-300"
                               }`}
